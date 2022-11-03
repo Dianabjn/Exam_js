d54be3426a53ec10ffe291cf0b4efb2cf68b6605
Barrientos Ruiz <dbarrientosruiz@deloitte.com>
2022-11-03 16:26:48 -0600

    all

---
+++ src/Components/Users/UsersList.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import Card from '../UI/Card';
+import styles from './UsersList.module.css';
+
+const UsersList = (props) => {
+    const [updateIsShown, setUpdateIsShown] = useState(false);
+
+    const showUpdateHandler = () => {
+      setUpdateIsShown(true);
+    };
+  
+    const hideUpdateHandler = () => {
+      setUpdateIsShown(false);
+    };
+    return (
+        <Card className={styles.users}>
+            <ul>
+                {props.users.map((user) => (
+                    <li key={user.id}>
+                        <div>
+                            {user.name} ({user.age} years old)
+                        </div>   
+                        <button onClick={showUpdateHandler}>Udpate</button>
+                        <button>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </Card>
+    );
+};
+
+export default UsersList;
